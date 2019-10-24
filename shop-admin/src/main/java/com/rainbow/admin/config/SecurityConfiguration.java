8d3604df2d76f3a49685fca37e18b7f30420a8bd
lujinwei <lujinwei@baijiahulian.com>
2019-10-24 19:55:00 +0800

    security修正

--- shop-admin/src/main/java/com/rainbow/admin/config/SecurityConfiguration.java
+++ shop-admin/src/main/java/com/rainbow/admin/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.rainbow.admin.config;
 
+import com.rainbow.admin.module.JwtAuthenticationTokenFilter;
 import com.rainbow.admin.module.RestAuthenticationEntryPoint;
 import com.rainbow.admin.module.RestfulAccessDeniedHandler;
 import com.rainbow.admin.service.IAdministratorService;
@@ -43,78 +44,68 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
 
 
-//    @Override
-//    protected void configure(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity.csrf()// 由于使用的是JWT，我们这里不需要csrf
-//                .disable()
-//                .sessionManagement()// 基于token，所以不需要session
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers(HttpMethod.GET, // 允许对于网站静态资源的无授权访问
-//                        "/",
-//                        "/*.html",
-//                        "/favicon.ico",
-//                        "/**/*.html",
-//                        "/**/*.css",
-//                        "/**/*.js",
-//                        "/swagger-resources/**",
-//                        "/v2/api-docs/**",
-//                        "/webjars/springfox-swagger-ui/**"
-//                )
-//                .permitAll()
-//                .antMatchers("/admin/login", "/admin/register")// 对登录注册要允许匿名访问
-//                .permitAll()
-//                .antMatchers(HttpMethod.OPTIONS)//跨域请求会先进行一次options请求
-//                .permitAll()
-////                .antMatchers("/**")//测试时全部运行访问
-////                .permitAll()
-//                .anyRequest()// 除上面外的所有请求全部需要鉴权认证
-//                .authenticated();
-//        // 禁用缓存
-//        httpSecurity.headers().cacheControl();
-//        // 添加JWT filter
-//        httpSecurity.addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-//        //添加自定义未授权和未登录结果返回
-//        httpSecurity.exceptionHandling()
-//                .accessDeniedHandler(restfulAccessDeniedHandler)
-//                .authenticationEntryPoint(restAuthenticationEntryPoint);
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService())
-//                .passwordEncoder(passwordEncoder());
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        //获取登录用户信息
-//        return username -> {
-//            UmsAdmin admin = adminService.getAdminByUsername(username);
-//            if (admin != null) {
-//                List<UmsPermission> permissionList = adminService.getPermissionList(admin.getId());
-//                return new AdminUserDetails(admin,permissionList);
-//            }
-//            throw new UsernameNotFoundException("用户名或密码错误");
-//        };
-//    }
-//
-//    @Bean
-//    public JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter(){
-//        return new JwtAuthenticationTokenFilter();
-//    }
-//
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf()// 由于使用的是JWT，我们这里不需要csrf
+                .disable()
+                .sessionManagement()// 基于token，所以不需要session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, // 允许对于网站静态资源的无授权访问
+                        "/",
+                        "/*.html",
+                        "/favicon.ico",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js"
+                )
+                .permitAll()
+                .antMatchers("/administrator/login", "/administrator/register")// 对登录注册要允许匿名访问
+                .permitAll()
+                .antMatchers("/**")     //测试时全部运行访问
+                .permitAll()
+                .anyRequest()                       // 除上面外的所有请求全部需要鉴权认证
+                .authenticated();
+        // 禁用缓存
+        httpSecurity.headers().cacheControl();
+        // 添加JWT filter
+        httpSecurity.addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        //添加自定义未授权和未登录结果返回
+        httpSecurity.exceptionHandling()
+                .accessDeniedHandler(restfulAccessDeniedHandler)
+                .authenticationEntryPoint(restAuthenticationEntryPoint);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        //获取登录用户信息
+        return username -> {
+            UmsAdmin admin = adminService.getAdminByUsername(username);
+            if (admin != null) {
+                List<UmsPermission> permissionList = adminService.getPermissionList(admin.getId());
+                return new AdminUserDetails(admin,permissionList);
+            }
+            throw new UsernameNotFoundException("用户名或密码错误");
+        };
+    }
+
+    @Bean
+    public JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter() {
+        return new JwtAuthenticationTokenFilter();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
 
 
---
+++ shop-admin/src/main/java/com/rainbow/admin/module/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,23 @@
+package com.rainbow.admin.module;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * JWT登陆授权过滤器
+ *
+ * @author lujinwei
+ * @since 2019-10-24
+ */
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
